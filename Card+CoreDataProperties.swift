d9dd117ad2ba0a3833e8acb566eb62cd5d5550d1
Keith <keith.ong5000@gmail.com>
2024-05-02 13:53:28 -0700

    re add missing model file

--- Card+CoreDataProperties.swift
+++ Card+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Card+CoreDataProperties.swift
 //  FlashCarta
 //
-//  Created by Keith Ong on 2024-04-13.
+//  Created by Keith Ong on 2024-05-02.
 //
 //
 
@@ -16,15 +16,15 @@ extension Card {
         return NSFetchRequest<Card>(entityName: "Card")
     }
 
-    @NSManaged public var exampleTranslation: String
-    @NSManaged public var example: String
+    @NSManaged public var exampleTranslation: String?
+    @NSManaged public var example: String?
     @NSManaged public var animation: String?
-    @NSManaged public var word: String
+    @NSManaged public var word: String?
     @NSManaged public var rank: Int64
-    @NSManaged public var partOfSpeech: String
+    @NSManaged public var partOfSpeech: String?
     @NSManaged public var nextReview: Date?
     @NSManaged public var difficulty: Int64
-    @NSManaged public var definition: String
+    @NSManaged public var definition: String?
 
 }
 
