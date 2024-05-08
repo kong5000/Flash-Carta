cc89d8f719acd9d41f7cdc6333be959943934954
Keith <keith.ong5000@gmail.com>
2024-05-08 07:57:42 -0700

    added confirmation dialog to settings screen

--- FlashCarta/Features/Settings/View/SettingsView.swift
+++ FlashCarta/Features/Settings/View/SettingsView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @AppStorage("repetitionFactor") private var selectedRepetitionFactor = 3
+    @State private var showConfirmation = false
+    var spaceRepetitionFactor = [2,3,4,5]
+    @State private var selectedFactor = 2
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack{
+                Text("Repetition factor")
+                Picker("Choose repetion factor", selection: $selectedRepetitionFactor) {
+                    ForEach(spaceRepetitionFactor, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+            }
+            Button(action: {
+                print("reset progress")
+                showConfirmation = true
+            }, label: {
+                Text("Reset your progress")
+            })
+            .confirmationDialog("Confirm Reset", isPresented: $showConfirmation, actions: {
+                Button("Confirm") {
+                    print("RESET")
+                }
+            }, message: {
+                Text("Your progress will be deleted")
+            })
+            .onChange(of: selectedRepetitionFactor) { oldValue, newValue in
+                print("UPDATE USER SETTING", oldValue, newValue)
+            }
+            Text("You selected: \(selectedRepetitionFactor)")
+        }
     }
 }
 
